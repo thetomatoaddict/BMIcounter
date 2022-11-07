3d6dcca4c31be47857d3b3bcf65e842fe9ae4ea8
sjlee <thetomatoaddict@gmail.com>
2022-11-07 15:19:31 +0900

    버튼수정

--- BMIcounter/ViewController.swift
+++ BMIcounter/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func btnBMI(_ sender: UIButton) {
+        txtResult.layer.cornerRadius = 7
         if txtHeight.text == "" || txtWeight.text == "" {
             print("input error")
             txtResult.text = "키와 체중을 입력하세요."
@@ -26,28 +27,35 @@ class ViewController: UIViewController {
         else {
         var height = Double(txtHeight.text!)!
         var weight = Double(txtWeight.text!)!
-        print(height,weight)
+        var color = UIColor.white
+            print(height,weight)
         
         let bmi = weight/(height*height*0.0001)
         let shortenedBMI = String(format: "%.1f", bmi)
         var body = ""
         if bmi >= 40 {
             body = "3단계 비만"
+            color = UIColor(displayP3Red: 0.4, green: 0.0, blue: 0.0, alpha: 1.0)
         }
         else if bmi >= 30 && bmi < 40 {
             body = "2단계 비만"
+            color = UIColor(displayP3Red: 0.7, green: 0.0, blue: 0.0, alpha: 1.0)
         }
         else if bmi >= 25 && bmi < 30 {
             body = "1단계 비만"
+            color = UIColor(displayP3Red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
         }
             else if bmi >= 18.5 && bmi < 25 {
             body = "정상"
+                color = UIColor(displayP3Red: 0.0, green: 0.7, blue: 0.3		, alpha: 1.0)
         }
         else{
             body = "저체중"
+            color = UIColor(displayP3Red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
         }
         
         txtResult.text = "BMI:\(shortenedBMI), 판정:\(body)" //실제출력부분
+        txtResult.backgroundColor = color
         print("BMI:\(shortenedBMI), 판정:\(body)")
             
     }
